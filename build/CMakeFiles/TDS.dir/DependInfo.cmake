71f698794c925e8e14aa03dbf3319383dbd64925
jayjaewonyoo <jy4763@gmail.com>
2018-12-25 03:21:47 -0500

    Added prototype ADS file creation

---
+++ build/CMakeFiles/TDS.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jayyoo/Documents/TDS/src/delta.cpp" "/home/jayyoo/Documents/TDS/build/CMakeFiles/TDS.dir/src/delta.cpp.o"
+  "/home/jayyoo/Documents/TDS/src/des.cpp" "/home/jayyoo/Documents/TDS/build/CMakeFiles/TDS.dir/src/des.cpp.o"
+  "/home/jayyoo/Documents/TDS/src/event.cpp" "/home/jayyoo/Documents/TDS/build/CMakeFiles/TDS.dir/src/event.cpp.o"
+  "/home/jayyoo/Documents/TDS/src/io.cpp" "/home/jayyoo/Documents/TDS/build/CMakeFiles/TDS.dir/src/io.cpp.o"
+  "/home/jayyoo/Documents/TDS/src/main.cpp" "/home/jayyoo/Documents/TDS/build/CMakeFiles/TDS.dir/src/main.cpp.o"
+  "/home/jayyoo/Documents/TDS/src/spec.cpp" "/home/jayyoo/Documents/TDS/build/CMakeFiles/TDS.dir/src/spec.cpp.o"
+  "/home/jayyoo/Documents/TDS/src/tds.cpp" "/home/jayyoo/Documents/TDS/build/CMakeFiles/TDS.dir/src/tds.cpp.o"
+  "/home/jayyoo/Documents/TDS/src/transition.cpp" "/home/jayyoo/Documents/TDS/build/CMakeFiles/TDS.dir/src/transition.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/TDS.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TDS.dir/src/delta.cpp.o"
+  "CMakeFiles/TDS.dir/src/des.cpp.o"
+  "CMakeFiles/TDS.dir/src/event.cpp.o"
+  "CMakeFiles/TDS.dir/src/io.cpp.o"
+  "CMakeFiles/TDS.dir/src/main.cpp.o"
+  "CMakeFiles/TDS.dir/src/spec.cpp.o"
+  "CMakeFiles/TDS.dir/src/tds.cpp.o"
+  "CMakeFiles/TDS.dir/src/transition.cpp.o"
+  "TDS.pdb"
+  "TDS"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TDS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
